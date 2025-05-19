bbae7e9e8f3bdd4500146532a8bea889afca3939
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2025-05-19 12:21:27 -0700

    Initial set-up

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+#  Provider
+provider "aws" {
+  region = "ca-central-1" # change region
+}
+
+#  S3 Bucket
+resource "aws_s3_bucket" "static_site" {
+  bucket = "my-terraform-site-bucket-123456" # make bucket name unique
+}
+
+#  S3 Bucket Policy
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "PublicReadGetObject",
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.static_site.id}/*"
+      }
+    ]
+  })
+}
+
+# S3 Bucket Website Configuration
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.static_site.id
+  index_document {
+    suffix = "index.html"
+  }
+}
